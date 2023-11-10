abf2e84658f8744593e406f98eac4b76431e3e22
Roger Davis <roger.davis86@yahoo.com>
2023-11-10 14:12:45 +0000

    Create print_board method for displaying the Battle Boards.

--- run.py
+++ run.py
@@ -5,3 +5,11 @@ class BattleBoard:
 """The BattleBoard class is called to create the player_boards, created and implemented in the GameLogic class"""
     def__init__(self):
         self.board=[['']*8 for_in range(8)]
+
+    def print_board(self):
+        print(' 12345678')
+        print(' ********')
+        row_num=1
+        for row in self.board:
+            print("%d|%s"%(row_num,"|".join(row)))
+            row_num +=1
